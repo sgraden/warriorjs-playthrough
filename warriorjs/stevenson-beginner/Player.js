ecab3e2a9aec7c41eb7689b27683684f022ac7d2
Steven Raden <stevengraden@gmail.com>
2016-07-08 21:17:04 -0700

    woot to level 5? need to figure out how to avoid archers

--- warriorjs/stevenson-beginner/Player.js
+++ warriorjs/stevenson-beginner/Player.js
@@ -1,5 +1,35 @@
+'esversion: 6';
+
 class Player {
-  playTurn(warrior) {
-    // Cool code goes here
-  }
+    constructor () {
+        this.health = 20;
+    }
+
+    playTurn (warrior) {
+        var direction = this.checkAround(warrior);
+        this.action(warrior, direction);
+        this.health = warrior.health();
+    }
+
+    checkAround (warrior) {
+        if (warrior.feel('forward')) {   //forward
+            return 'forward';
+        } else {                //backward
+            return 'backward';
+        }
+    }
+
+    action (warrior, direction) {
+        var feel = warrior.feel(direction);
+        if (feel.isEnemy()) {           //enemy
+            warrior.attack();
+        } else if (feel.isCaptive()) {  //captive
+            warrior.rescue();
+        } else if (warrior.health() < 20 && (warrior.health() >= this.health)) {
+            warrior.rest();
+        } else {                        //walk
+            warrior.walk(direction);
+        }
+    }
+
 }
