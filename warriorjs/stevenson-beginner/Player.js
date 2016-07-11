c65938b2b029ed8dd260bc548efd345a878453e2
Steven Raden <stevengraden@gmail.com>
2016-07-10 17:45:03 -0700

    rewrote a lot of stuff to better optimize, but found out that the actions don't handle themselves. rewriting again

--- warriorjs/stevenson-beginner/Player.js
+++ warriorjs/stevenson-beginner/Player.js
@@ -3,33 +3,103 @@
 class Player {
     constructor () {
         this.health = 20;
+        this.charge = false;
+        this.direction = 'backward';
+        this.acted = false;
+
     }
 
     playTurn (warrior) {
-        var direction = this.checkAround(warrior);
-        this.action(warrior, direction);
+        this.acted = false;
+        if (warrior.health < 10) {
+            this.charge = false;
+        }
+        this.checkDirection(warrior);   //Set direction to go
+        if (!this.acted) {
+            this.checkCaptive(warrior);     //check if you found a captiv
+        }
+        console.log(this.charge);
+        if (!this.acted && !this.charge) { //if not charging then check if need to rest
+            this.checkRest(warrior);
+        }
+        if (!this.acted) {
+            this.checkCharge(warrior);
+        }
+        if (!this.acted) {
+            this.checkAttack(warrior);
+        }
+        if (!this.acted) {
+            warrior.walk(this.direction); //walk!
+        }
         this.health = warrior.health();
     }
 
-    checkAround (warrior) {
-        if (warrior.feel('forward')) {   //forward
-            return 'forward';
-        } else {                //backward
-            return 'backward';
+    checkDirection (warrior) {
+        var feel = warrior.feel(this.direction);
+        if (feel.isWall()) {
+            this.oppositeDirection();
         }
     }
 
-    action (warrior, direction) {
-        var feel = warrior.feel(direction);
-        if (feel.isEnemy()) {           //enemy
-            warrior.attack();
-        } else if (feel.isCaptive()) {  //captive
-            warrior.rescue();
-        } else if (warrior.health() < 20 && (warrior.health() >= this.health)) {
-            warrior.rest();
-        } else {                        //walk
-            warrior.walk(direction);
+    checkCaptive (warrior) {
+        var feel = warrior.feel(this.direction);
+        if (feel.isCaptive()) {
+            warrior.rescue(this.direction);
+            this.acted = true;
         }
     }
+
+    checkRest (warrior) {
+        console.log('check rest');
+        if (warrior.health < this.health) { //taking damage
+            this.oppositeDirection(); // turn around
+            warrior.walk(this.direction);
+            this.acted = true;
+        } else if (warrior.health < 20) {
+            console.log('resting');
+            warrior.rest();
+            this.acted = true;
+        }
+        if (this.acted && warrior.health == 20) { //all healed turnaround and run
+            this.oppositeDirection();
+        }
+    }
+
+    checkCharge (warrior) {
+        if (warrior.health == 20) {
+            this.charge = true;
+        }
+    }
+
+    checkAttack (warrior) {
+        var feel = warrior.feel(this.direction);
+        if (feel.isEnemy()) {
+            warrior.attack(this.direction);
+            this.acted = true;
+        }
+    }
+
+    oppositeDirection () {
+        if (this.direction == 'forward') {
+            this.direction = 'backward';
+        } else {
+            this.direction = 'forward';
+        }
+    }
+
+    /**
+        check direction
+        check for captives
+            move towards
+            rescue
+        check for health
+            taking damage?
+                yes
+                    move opposite of damage
+                no
+                    rest to full health
+        check for attacking
+            charge
+    */
 
 }
